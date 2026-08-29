125cbf7e1bf8b49c9608fc8d4d4df8bcb6ab305a
Dalaa Khreis <288004092+dalaakhr@users.noreply.github.com>
2026-08-29 14:08:20 +0200

    Abhaken-Funktion fuer gekochte Rezepte hinzugefuegt

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -7,9 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-list.html',
 })
 export class RecipeList {
-    rezepte = [
-      { titel: 'Ceaser Salad', kategorie: 'Vorspeise', zeit: 15 },
-      { titel: 'Lasagne', kategorie: 'Hauptgericht', zeit: 30 },
-      { titel: 'Tiramisu', kategorie: 'Dessert', zeit: 20 }
-    ];
+  rezepte = [
+    { titel: 'Ceaser Salad', kategorie: 'Vorspeise', zeit: 15, gemacht: true },
+    { titel: 'Lasagne', kategorie: 'Hauptgericht', zeit: 30, gemacht: false },
+    { titel: 'Tiramisu', kategorie: 'Dessert', zeit: 20, gemacht: false }
+  ];
+  toggleGemacht(rezept: any) {
+    rezept.gemacht = !rezept.gemacht;
+  }
 }
