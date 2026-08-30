9e086d595cc792057869b817c0c3e36c9f9a1e31
Dalaa Khreis <288004092+dalaakhr@users.noreply.github.com>
2026-08-30 20:43:46 +0200

    Bearbeiten-Funktion fuer Rezepte hinzugefuegt

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-recipe-list',
   styleUrl: './recipe-list.css',
   templateUrl: './recipe-list.html',
 })
 export class RecipeList {
   rezepte = [
-    { titel: 'Ceaser Salad', kategorie: 'Vorspeise', zeit: 15, gemacht: true },
-    { titel: 'Lasagne', kategorie: 'Hauptgericht', zeit: 30, gemacht: false },
-    { titel: 'Tiramisu', kategorie: 'Dessert', zeit: 20, gemacht: false }
+    { titel: 'Ceaser Salad', kategorie: 'Vorspeise', zeit: 15, gemacht: true, bearbeitung: false },
+    { titel: 'Lasagne', kategorie: 'Hauptgericht', zeit: 30, gemacht: false, bearbeitung: false },
+    { titel: 'Tiramisu', kategorie: 'Dessert', zeit: 20, gemacht: false, bearbeitung: false  }
   ];
+    neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false , bearbeitung: false };
   toggleGemacht(rezept: any) {
     rezept.gemacht = !rezept.gemacht;
+  }
+    rezeptHinzufuegen() {
+    this.rezepte.push({ ...this.neuesRezept });
+    this.neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false , bearbeitung: false };
+  }
+    bearbeiten(rezept: any) {
+    rezept.bearbeitung = !rezept.bearbeitung;
   }
 }
