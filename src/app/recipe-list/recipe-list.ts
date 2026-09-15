c3fc35d14e5e1fead0e386c08f98d0e33b13baaf
Dalaa Khreis <288004092+dalaakhr@users.noreply.github.com>
2026-09-15 11:13:09 +0200

    GET-Anbindung: Rezepte werden vom Backend geladen

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RecipeService } from '../shared/recipe';
 
 @Component({
   imports: [FormsModule],
@@ -7,24 +8,29 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './recipe-list.css',
   templateUrl: './recipe-list.html',
 })
-export class RecipeList {
-  rezepte = [
-    { titel: 'Ceaser Salad', kategorie: 'Vorspeise', zeit: 15, gemacht: true, bearbeitung: false },
-    { titel: 'Lasagne', kategorie: 'Hauptgericht', zeit: 30, gemacht: false, bearbeitung: false },
-    { titel: 'Tiramisu', kategorie: 'Dessert', zeit: 20, gemacht: false, bearbeitung: false  }
-  ];
-    neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false , bearbeitung: false };
+export class RecipeList implements OnInit {
+  private rs = inject(RecipeService);
+
+  rezepte: any[] = [];
+  neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false, bearbeitung: false };
+
+  ngOnInit(): void {
+    this.rs.getAll()
+      .then(response => this.rezepte = response)
+      .then(rezepte => console.log('rezepte in RecipeList: ', rezepte));
+  }
+
   toggleGemacht(rezept: any) {
     rezept.gemacht = !rezept.gemacht;
   }
-    rezeptHinzufuegen() {
+  rezeptHinzufuegen() {
     this.rezepte.push({ ...this.neuesRezept });
-    this.neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false , bearbeitung: false };
+    this.neuesRezept = { titel: '', kategorie: '', zeit: 0, gemacht: false, bearbeitung: false };
   }
-    bearbeiten(rezept: any) {
+  bearbeiten(rezept: any) {
     rezept.bearbeitung = !rezept.bearbeitung;
   }
-    loeschen(rezept: any) {
+  loeschen(rezept: any) {
     this.rezepte = this.rezepte.filter(r => r !== rezept);
   }
-}
+} 
